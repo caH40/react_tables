9160ba6ce47782fef866fa2824c9d79765c8d448
caH40 <kmvsancho@gmail.com>
2022-11-17 09:25:37 +0300

    added a popup

--- src/Components/UI/Tables/StageEdit/TableStageEdit.jsx
+++ src/Components/UI/Tables/StageEdit/TableStageEdit.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -7,20 +7,35 @@ import { getResults } from '../../../../api/getResults';
 import TableStageEditBody from './TableStageEditBody';
 import classes from '../Table.module.css';
 import { titlesStageEdit } from '../titles';
+import { useTelegram } from '../../../../hooks/useTelegram';
 
 const TableStageEdit = () => {
 	const [results, setResults] = useState([]);
-	const [popup, setPopup] = useState();
+	const [popup, setPopup] = useState('');
+
+	const { params } = useParams();
+	const { tg } = useTelegram();
+
 	console.log('==================new render===================');
-	let { params } = useParams();
 
 	useEffect(() => {
 		getResults(params).then(data => setResults(data));
 	}, []);
 
+	useMemo(() => {
+		//при нажатии на Х callback не срабатывает если не указать id
+		if (popup)
+			tg.showPopup(
+				{ title: 'Выполнена операция', message: popup, buttons: [{ type: 'close', id: 1 }] },
+				() => {
+					setPopup('');
+				}
+			);
+	}, [popup]);
+
 	return (
 		<div>
-			<h2>{popup}</h2>
+			{/* {popup ? <p>{popup}</p> : ''} */}
 			<table className={classes.myTable}>
 				<caption>{results[0]?.title}</caption>
 				<thead>
