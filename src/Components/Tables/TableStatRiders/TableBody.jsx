f39a617da6fa591368a059d589ceba7ab030a70b
Alexander Berezhnev <freelancerbav@gmail.com>
2022-12-02 10:47:38 +0300

    added a counting clicks in statsRiders

--- src/Components/Tables/TableStatRiders/TableBody.jsx
+++ src/Components/Tables/TableStatRiders/TableBody.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import cls from '../Table.module.css';
 import { tdStages, tdTime } from '../../../Utils/statistics';
 import { tdRider } from '../../../Utils/table';
+import Spinner from '../../UI/Spinner/Spinner';
 
-const TableBody = ({ results }) => {
+const TableBody = ({ results, isLoading }) => {
 	return (
 		<tbody>
+			<tr>
+				<td colSpan={5}>
+					<Spinner isLoading={isLoading} />
+				</td>
+			</tr>
 			{results.map((result, index) => {
 				return (
 					<tr key={result._id}>
