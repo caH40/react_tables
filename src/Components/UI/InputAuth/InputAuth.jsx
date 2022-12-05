5231110a7439484baa563a130156238646cd0402
Alexander Berezhnev <freelancerbav@gmail.com>
2022-12-05 15:54:00 +0300

    added the hook useAuth part2

--- src/Components/UI/InputAuth/InputAuth.jsx
+++ src/Components/UI/InputAuth/InputAuth.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import Button from '../Button/Button';
+import cls from './InputAuth.module.css';
 
 const InputAuth = ({ query, setQuery, getHashPassword, password }) => {
 	return password ? (
 		''
 	) : (
-		<form>
+		<form style={{ display: 'flex', justifyContent: 'left' }}>
 			<input
+				className={cls.MyInput}
+				type="text"
+				autoComplete="username"
+				name="username"
+				id="username"
+				style={{ display: 'none' }}
+			/>
+			<input
+				className={cls.MyInput}
 				value={query}
 				onChange={e => setQuery(e.target.value)}
 				type={'password'}
 				autoComplete="current-password"
 				id="current-password"
+				name="password"
 			/>
-			<button onClick={getHashPassword}>Sing in</button>
+
+			<Button sendForm={getHashPassword}>Sing in</Button>
 		</form>
 	);
 };
