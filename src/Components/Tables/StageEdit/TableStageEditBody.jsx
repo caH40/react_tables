a4d58c49bc42b39deeff94e40ee24472c04aa90f
Alexander Berezhnev <freelancerbav@gmail.com>
2022-12-06 19:43:13 +0300

    fixed the bug with women category WA,WB

--- src/Components/Tables/StageEdit/TableStageEditBody.jsx
+++ src/Components/Tables/StageEdit/TableStageEditBody.jsx
@@ -9,106 +9,106 @@ import Spinner from '../../UI/Spinner/Spinner';
 import CheckboxDq from '../../UI/CheckboxDq/CheckboxDq';
 
 const TableStageEditBody = ({ results, setPopup, setResults, isLoading, telegramId, password }) => {
-	return (
-		<tbody>
-			<tr>
-				<td colSpan={10}>
-					<Spinner isLoading={isLoading} />
-				</td>
-			</tr>
-			{results.map(result => {
-				const category = (
-					<div>
-						{
-							<SelectCategory
-								results={results}
-								setResults={setResults}
-								setPopup={setPopup}
-								zwiftId={result.zwiftRiderId}
-								stageId={result.stageId}
-								defaultValue={result.category}
-								key={result._id}
-								telegramId={telegramId}
-								password={password}
-							/>
-						}
-					</div>
-				);
-				const penalty = (
-					<div>
-						{
-							<SelectPenalty
-								result={result}
-								results={results}
-								setResults={setResults}
-								setPopup={setPopup}
-								zwiftId={result.zwiftRiderId}
-								defaultValue={result.penalty.powerUp}
-								key={result._id}
-								telegramId={telegramId}
-								password={password}
-							/>
-						}
-					</div>
-				);
-				const selectSprint = result.quantitySprints.map((elm, index) => (
-					<td key={elm}>
-						<SelectSprintPoints
-							name={elm}
-							index={index}
-							result={result}
-							results={results}
-							setResults={setResults}
-							setPopup={setPopup}
-							telegramId={telegramId}
-							password={password}
-						/>
-					</td>
-				));
-				const selectMountain = result.quantityMountains.map((elm, index) => (
-					<td key={elm}>
-						<SelectMountainPoints
-							name={elm}
-							index={index}
-							result={result}
-							results={results}
-							setResults={setResults}
-							setPopup={setPopup}
-							telegramId={telegramId}
-							password={password}
-						/>
-					</td>
-				));
-				return (
-					<tr key={result._id}>
-						<td>
-							<CheckboxDq
-								result={result}
-								results={results}
-								setResults={setResults}
-								setPopup={setPopup}
-								telegramId={telegramId}
-								password={password}
-							/>
-						</td>
-						<td>{penalty}</td>
-						<td>{result.placeAbsolute}</td>
-						<td>{category}</td>
-						<td>{valueMax(result.watt, 'w')}</td>
-						<td>{valueMax(result.wattPerKg, 'w/kg')}</td>
-						<td>{tdRider(result.name, result.imageSrc)}</td>
-						{selectSprint}
-						{selectMountain}
-						<td>{tdLinkZP(result.zwiftRiderId)}</td>
-						<td>{tdTime(result.time)}</td>
-						<td>{valueMax(result.avgHeartRate, 'bpm')}</td>
-						<td>{valueMax(result.weightInGrams, 'kg')}</td>
-						<td>{valueMax(result.heightInCentimeters, 'cm')}</td>
-					</tr>
-				);
-			})}
-		</tbody>
-	);
+  return (
+    <tbody>
+      <tr>
+        <td colSpan={10}>
+          <Spinner isLoading={isLoading} />
+        </td>
+      </tr>
+      {results.map((result) => {
+        const category = (
+          <div>
+            {
+              <SelectCategory
+                results={results}
+                setResults={setResults}
+                setPopup={setPopup}
+                zwiftId={result.zwiftRiderId}
+                stageId={result.stageId}
+                defaultValue={result.category}
+                key={result._id}
+                telegramId={telegramId}
+                password={password}
+              />
+            }
+          </div>
+        );
+        const penalty = (
+          <div>
+            {
+              <SelectPenalty
+                result={result}
+                results={results}
+                setResults={setResults}
+                setPopup={setPopup}
+                zwiftId={result.zwiftRiderId}
+                defaultValue={result.penalty.powerUp}
+                key={result._id}
+                telegramId={telegramId}
+                password={password}
+              />
+            }
+          </div>
+        );
+        const selectSprint = result.quantitySprints.map((elm, index) => (
+          <td key={elm}>
+            <SelectSprintPoints
+              name={elm}
+              index={index}
+              result={result}
+              results={results}
+              setResults={setResults}
+              setPopup={setPopup}
+              telegramId={telegramId}
+              password={password}
+            />
+          </td>
+        ));
+        const selectMountain = result.quantityMountains.map((elm, index) => (
+          <td key={elm}>
+            <SelectMountainPoints
+              name={elm}
+              index={index}
+              result={result}
+              results={results}
+              setResults={setResults}
+              setPopup={setPopup}
+              telegramId={telegramId}
+              password={password}
+            />
+          </td>
+        ));
+        return (
+          <tr key={result._id}>
+            <td>
+              <CheckboxDq
+                result={result}
+                results={results}
+                setResults={setResults}
+                setPopup={setPopup}
+                telegramId={telegramId}
+                password={password}
+              />
+            </td>
+            <td>{penalty}</td>
+            <td>{result.placeAbsolute}</td>
+            <td>{category}</td>
+            <td>{valueMax(result.watt, 'w')}</td>
+            <td>{valueMax(result.wattPerKg, 'w/kg')}</td>
+            <td>{tdRider(result.name, result.imageSrc)}</td>
+            {selectSprint}
+            {selectMountain}
+            <td>{tdLinkZP(result.zwiftRiderId)}</td>
+            <td>{tdTime(result.time)}</td>
+            <td>{valueMax(result.avgHeartRate, 'bpm')}</td>
+            <td>{valueMax(result.weightInGrams, 'kg')}</td>
+            <td>{valueMax(result.heightInCentimeters, 'cm')}</td>
+          </tr>
+        );
+      })}
+    </tbody>
+  );
 };
 
 export default TableStageEditBody;
--- src/Components/UI/SelectCategory/SelectCategory.jsx
+++ src/Components/UI/SelectCategory/SelectCategory.jsx
@@ -2,45 +2,57 @@ import React from 'react';
 import { postStageEdit } from '../../../api/stage-edit';
 
 const SelectCategory = ({
-	defaultValue,
-	zwiftId,
-	stageId,
-	setPopup,
-	results,
-	setResults,
-	telegramId,
-	password,
+  defaultValue,
+  zwiftId,
+  stageId,
+  setPopup,
+  results,
+  setResults,
+  telegramId,
+  password,
 }) => {
-	const color = { A: '#dc4119', B: '#58c34e', C: '#3ec0e9', W: '#9422ae' };
+  const color = {
+    A: '#dc4119',
+    B: '#58c34e',
+    C: '#3ec0e9',
+    W: '#9422ae',
+    WA: '#9422ae',
+    WB: '#9422ae',
+  };
 
-	const changeCategory = e => {
-		const newCategory = e.target.value;
+  const changeCategory = (e) => {
+    const newCategory = e.target.value;
 
-		let r = [];
-		results.forEach(result => {
-			if (result.zwiftRiderId === zwiftId) result.category = newCategory;
-			r.push(result);
-		});
-		setResults(r);
+    let r = [];
+    results.forEach((result) => {
+      if (result.zwiftRiderId === zwiftId) result.category = newCategory;
+      r.push(result);
+    });
+    setResults(r);
 
-		postStageEdit(newCategory, zwiftId, stageId, telegramId, password).then(data => setPopup(data));
-	};
+    postStageEdit(newCategory, zwiftId, stageId, telegramId, password).then((data) =>
+      setPopup(data)
+    );
+  };
 
-	return (
-		<div>
-			<select
-				onChange={changeCategory}
-				style={{ background: color[defaultValue] }}
-				name="category"
-				size="1"
-				defaultValue={defaultValue}>
-				<option style={{ background: color.A }} value="A" label="A" />
-				<option style={{ background: color.B }} value="B" label="B" />
-				<option style={{ background: color.C }} value="C" label="C" />
-				<option style={{ background: color.W }} value="W" label="W" />
-			</select>
-		</div>
-	);
+  return (
+    <div>
+      <select
+        onChange={changeCategory}
+        style={{ background: color[defaultValue] }}
+        name="category"
+        size="1"
+        defaultValue={defaultValue}
+      >
+        <option style={{ background: color.A }} value="A" label="A" />
+        <option style={{ background: color.B }} value="B" label="B" />
+        <option style={{ background: color.C }} value="C" label="C" />
+        <option style={{ background: color.W }} value="W" label="W" />
+        <option style={{ background: color.W }} value="WA" label="WA" />
+        <option style={{ background: color.W }} value="WB" label="WB" />
+      </select>
+    </div>
+  );
 };
 
 export default SelectCategory;
