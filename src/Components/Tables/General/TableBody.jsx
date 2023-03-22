04043595e8bdf4d1b758ae79cf61f43a06f4e9d7
Alexander Berezhnev <freelancerbav@gmail.com>
2023-03-22 17:39:39 +0300

    added active tr in general tables

--- src/Components/Tables/General/TableBody.jsx
+++ src/Components/Tables/General/TableBody.jsx
@@ -4,8 +4,13 @@ import cls from '../Table.module.css';
 import { tdPointsStage, tdPointsTotal } from '../../../Utils/general';
 import { tdRider } from '../../../Utils/table';
 import Spinner from '../../UI/Spinner/Spinner';
+import { useNavigate } from 'react-router-dom';
 
 const TableBody = ({ general, isLoading }) => {
+	const navigate = useNavigate();
+	const toProfile = zwiftId => {
+		navigate(`/profile/${zwiftId}/back`);
+	};
 	return (
 		<tbody>
 			<tr>
@@ -15,7 +20,7 @@ const TableBody = ({ general, isLoading }) => {
 			</tr>
 			{general.map(rider => {
 				return (
-					<tr key={rider.name}>
+					<tr key={rider.name} onClick={() => toProfile(rider.zwiftRiderId)} className={cls.trLink}>
 						<td className={cls.tdPointsStage}>{rider.sequence}</td>
 						<td className={cls.tdRiderStage}>{tdRider(rider.name, rider.imageSrc)}</td>
 						<td className={cls.tdPointsTotalStage}>{tdPointsTotal(rider)}</td>
